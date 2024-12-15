1df60a3376c7263085d3829109371408b36f9023
maurinei-miranda <maurinei.develop@gmail.com>
2024-12-15 03:53:48 -0300

    contact list

--- src/routes/ContactPage/contactPageComponent.jsx
+++ src/routes/ContactPage/contactPageComponent.jsx
@@ -1,9 +1,27 @@
 import PageTitle from "../../components/PageTitle/PageTitleComponent";
+import "./index.css";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
 
 function ContactPage() {
-    return (
-      <PageTitle valor="Página de Contatos"/>
-    )
-  }
+  return (
+    <>
+      <PageTitle valor="Página de Contatos" />
+      <div id="contact-list">
+        <ul>
+          <a href="https://wa.me/5511919214083">
+            <li>
+              <BsWhatsapp /> WhatsApp
+            </li>
+          </a>
+          <a href="https://www.instagram.com/gabifoxgames/">
+            <li>
+              <BsInstagram /> Instagram
+            </li>
+          </a>
+        </ul>
+      </div>
+    </>
+  );
+}
 
-  export default ContactPage;
+export default ContactPage;
